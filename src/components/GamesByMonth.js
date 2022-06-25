8e9194b73e7c533293211ccc0564cd3717dbf025
Robiul Hasan <muhammadrobiul502@gmail.com>
2022-06-25 13:07:05 +0600

    latest

--- src/components/GamesByMonth.js
+++ src/components/GamesByMonth.js
@@ -1,13 +1,14 @@
 import GamesCard from "./GamesCard";
 
 const GamesByMonth = ({ games }) => {
- 
+    const date = new Date();
+    const year = date.getFullYear()
   return (
     <>
-    {/* check if games has a value */}
+      {/* check if games has a value */}
       {games[1].length > 0 && (
         <div>
-          <h2 className="section-title">{games[0]} 2022</h2>
+          <h2 className="section-title">{games[0]} {year}</h2>
           <div>
             {/* loop games */}
             {games[1].map((game, index) => (
